7e1b17d7efa641fe01ed8b0e167b2f6c5b47bb50
amreenkaur306-hash <amreenkaur306@gmail.com>
2026-09-23 09:51:56 -0400

    Create Adding_two_strings.java

---
+++ Adding_two_strings.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       String first_name = "Amreen";
+       String last_name = "Kaur";
+       String full_name = first_name + last_name;
+       System.out.println(full_name);
+
+    }
+}
